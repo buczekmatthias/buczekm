a96289d0c585283efab8eb58891b040a1866ad8c
Maciej Buczek <Wchodz3WTo@gmail.com>
2023-09-21 14:45:43 +0200

    Stack mobile view

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-8 pt-2">
+  <div class="flex flex-col gap-8 py-2">
     <div class="flex justify-between items-center">
       <p class="flex flex-col gap-3">
         <span class="text-4xl font-semibold">Projects</span>
