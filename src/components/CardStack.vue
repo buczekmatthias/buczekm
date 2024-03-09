58531eca3d8078484bdf7500a6e309d8f46c7492
Hazy <Wchodz3WTo@gmail.com>
2024-03-09 10:58:28 +0100

    Stack grid change

--- src/components/CardStack.vue
+++ src/components/CardStack.vue
@@ -1,6 +1,6 @@
 <template>
   <CardComponent header="Stack" classes="md:col-span-full">
-    <div class="grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-6">
+    <div class="grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-5">
       <StackItem v-for="item in stack.items" :key="item.name" :item="item" classes="flex-col gap-8 p-6" />
     </div>
   </CardComponent>
